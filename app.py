f7b4df0144a9942766829b77a575de3dadca1c71
kanishkb1 <kanishkbarhanpurkar@yahoo.com>
2022-02-05 21:20:25 -0500

    simpler version

--- app.py
+++ app.py
@@ -1,45 +1,30 @@
-from flask import Flask, render_template,request,url_for
-from flask_bootstrap import Bootstrap 
 import nltk
-
-# NLP Packages
-from textblob import TextBlob,Word 
-import random 
-import time
+from flask import request
+from flask import jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
-Bootstrap(app)
 
 @app.route('/')
-def index():
+def my_form():
     return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    nltk.download('vader_lexicon')
+    from nltk.sentiment.vader import SentimentIntensityAnalyzer
+    sid = SentimentIntensityAnalyzer()
+    score = ((sid.polarity_scores(str(text))))['compound']
 
-@app.route('/analyse',methods=['POST'])
-def analyse():
-    start = time.time()
-    if request.method == 'POST':
-        rawtext = request.form['rawtext']
-		#NLP Stuff
-        blob = TextBlob(rawtext)
-        received_text2 = blob
-        blob_sentiment,blob_subjectivity = blob.sentiment.polarity ,blob.sentiment.subjectivity
-        number_of_tokens = len(list(blob.words))
-		# Extracting Main Points
-        nouns = list()
-        for word, tag in blob.tags:
-            if tag == 'NN':
-                nouns.append(word.lemmatize())
-                len_of_words = len(nouns)
-                rand_words = random.sample(nouns,len(nouns))
-                final_word = list()
-                for item in rand_words:
-                    word = Word(item).pluralize()
-                    final_word.append(word)
-                    summary = final_word
-                    end = time.time()
-                    final_time = end-start
-    return render_template('index.html',received_text = received_text2,number_of_tokens=number_of_tokens,blob_sentiment=blob_sentiment,blob_subjectivity=blob_subjectivity,summary=summary,final_time=final_time)
+    if(score > 0):
+        label = 'This sentence is positive'
+    elif(score == 0):
+        label = 'This sentence is neutral'
+    else:
+        label = 'This sentence is negative'
 
-if __name__ == '__main__':
-	app.run(debug=True)
+    return(render_template('index.html', variable=label))
+
+if __name__ == "__main__":
+    app.run(port='8088', threaded=False, debug=True)
